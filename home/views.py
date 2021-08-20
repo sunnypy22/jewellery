d4d715546ce7c7ae6b40045dd6b336215b652ad8
sunnypy22 <sunny.py22@gmail.com>
2021-08-20 12:00:13 +0530

    wishlist authentication

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from product.models import Category, Product
 from order.models import Wishlist
@@ -10,23 +11,26 @@ def index(request):
     cat = Category.objects.all()
     pro = Product.objects.all().order_by('-id')[0:6]
     feature = Product.objects.all().filter(featured=True)
+
     if request.method == "POST":
-
-        if "wishlist_form" in request.POST:
-            pro_id = request.POST.get('pro_id')
-            pro_name = request.POST.get('pro_name')
-            if Wishlist.objects.filter(wish_list_user = request.user,wish_list_product=pro_id).exists():
-                messages.warning(request, 'Product {} already exists in wishlist!'.format(pro_name))
-            else:
-                if request.user.is_authenticated:
-                    data = Wishlist.objects.create(wish_list_product_id=pro_id, wish_list_user=request.user,
-                                                   wish_list_status=True)
-
-                    data.save()
-                    return redirect("wishlist")
+        if request.user.is_authenticated:
+            if "wishlist_form" in request.POST:
+                pro_id = request.POST.get('pro_id')
+                pro_name = request.POST.get('pro_name')
+                if Wishlist.objects.filter(wish_list_user = request.user,wish_list_product=pro_id).exists():
+                    messages.warning(request, 'Product {} already exists in wishlist!'.format(pro_name))
                 else:
-                    return redirect("signin")
+                    if request.user.is_authenticated:
+                        data = Wishlist.objects.create(wish_list_product_id=pro_id, wish_list_user=request.user,
+                                                       wish_list_status=True)
 
+                        data.save()
+                        return redirect("wishlist")
+                    else:
+                        return redirect("signin")
+        else:
+            return redirect("signin")
     else:
         pass
+
     return render(request, 'index.html', {'cat': cat, 'pro': pro, 'feature': feature})
--- product/views.py
+++ product/views.py
@@ -51,22 +51,23 @@ def products(request, pid):
     else:
         pass
     if request.method == "POST":
-
-        if "wishlist_form" in request.POST:
-            pro_id = request.POST.get('pro_id')
-            pro_name = request.POST.get('pro_name')
-            if Wishlist.objects.filter(wish_list_user = request.user,wish_list_product=pro_id).exists():
-                messages.warning(request, 'Product {} already exists in wishlist!'.format(pro_name))
-            else:
-                if request.user.is_authenticated:
-                    data = Wishlist.objects.create(wish_list_product_id=pro_id, wish_list_user=request.user,
-                                               wish_list_status=True)
-
-                    data.save()
-                    return redirect("wishlist")
+        if request.user.is_authenticated:
+            if "wishlist_form" in request.POST:
+                pro_id = request.POST.get('pro_id')
+                pro_name = request.POST.get('pro_name')
+                if Wishlist.objects.filter(wish_list_user = request.user,wish_list_product=pro_id).exists():
+                    messages.warning(request, 'Product {} already exists in wishlist!'.format(pro_name))
                 else:
-                    return redirect("signin")
+                    if request.user.is_authenticated:
+                        data = Wishlist.objects.create(wish_list_product_id=pro_id, wish_list_user=request.user,
+                                                   wish_list_status=True)
 
+                        data.save()
+                        return redirect("wishlist")
+                    else:
+                        return redirect("signin")
+        else:
+            return redirect("signin")
     else:
         pass
     return render(request, 'collection-left.html',
@@ -78,4 +79,24 @@ def product_view(request, pid):
     pro = Product.objects.get(id=pid)
     color = Product_Color.objects.filter(color_key_id=pid)
     image = PostImage.objects.filter(post_id=pid)
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            if "wishlist_form" in request.POST:
+                pro_id = request.POST.get('pro_id')
+                pro_name = request.POST.get('pro_name')
+                if Wishlist.objects.filter(wish_list_user = request.user,wish_list_product=pro_id).exists():
+                    messages.warning(request, 'Product {} already exists in wishlist!'.format(pro_name))
+                else:
+                    if request.user.is_authenticated:
+                        data = Wishlist.objects.create(wish_list_product_id=pro_id, wish_list_user=request.user,
+                                                   wish_list_status=True)
+
+                        data.save()
+                        return redirect("wishlist")
+                    else:
+                        return redirect("signin")
+        else:
+            return redirect("signin")
+    else:
+        pass
     return render(request, 'product.html', {'pro': pro,'color':color,'image':image})
