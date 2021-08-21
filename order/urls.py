52223fa52d2745973c8069ae3a67b12e515f6f4c
sunnypy22 <sunny.py22@gmail.com>
2021-08-21 11:40:32 +0530

    update cart

--- order/urls.py
+++ order/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('wishlist/', views.wishlist, name="wishlist"),
     path('del_wishlist/<int:pid>', views.del_wishlist, name="del_wishlist"),
     path('cart/', views.cart, name="cart"),
+    path('update_cart/<int:pid>', views.update_cart, name="update_cart"),
     path('del_cart/<int:pid>', views.del_cart, name="del_cart"),
 
 ]
--- order/views.py
+++ order/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Wishlist, Cart
+from django.contrib import messages
+from product.models import Product_Color, PostImage
 
 
 # Create your views here.
@@ -20,6 +22,25 @@ def del_wishlist(request, pid):
 def cart(request):
     data = Cart.objects.filter(cart_user=request.user)
     return render(request, 'cart.html', {'data': data})
+
+
+def update_cart(request, pid):
+    try:
+        data = Cart.objects.get(id=pid, cart_user=request.user)
+        pro_id = data.cart_product.id
+        color = Product_Color.objects.filter(color_key_id=pro_id)
+        image = PostImage.objects.filter(post_id=pro_id)
+        if request.method == "POST":
+            quantity = request.POST.get('quantity')
+            cart_color_item = request.POST.get('cart_color_item')
+            Cart.objects.filter(cart_user = request.user).update(cart_quantity = quantity,cart_color = cart_color_item)
+            return redirect("cart")
+        else:
+            pass
+        return render(request, 'update_cart.html', {'pro': data, 'color': color, 'image': image})
+    except:
+        messages.error(request, 'You are unauthorized to access the link')
+        return redirect("cart")
 
 
 def del_cart(request, pid):
